0085837a397014276bb7b3bba93d911b5442db57
TheDestroyer5 <sharkyandted@gmail.com>
2023-09-08 18:10:48 +1200

    Pirate Mario Game Added

---
+++ Pirate Mario Game/code/game_data.py
@@ -0,0 +1,91 @@
+level_0 = {
+        "terrain": "./levels/0/level_0_terrain.csv",
+        "coins": "./levels/0/level_0_coins.csv",
+        "fg palms": "./levels/0/level_0_fg_palms.csv",
+        "bg palms": "./levels/0/level_0_bg_palms.csv",
+        "crates": "./levels/0/level_0_crates.csv",
+        "enemies": "./levels/0/level_0_enemies.csv",
+        "constraints": "./levels/0/level_0_constraints.csv",
+        "player": "./levels/0/level_0_player.csv",
+        "grass": "./levels/0/level_0_grass.csv", 
+        "node_pos": (110, 400),
+        "unlock": 1,
+        "node_graphics": "./graphics/overworld/0"}
+
+level_1 = {
+        "terrain": "./levels/1/level_1_terrain.csv",
+        "coins": "./levels/1/level_1_coins.csv",
+        "fg palms": "./levels/1/level_1_fg_palms.csv",
+        "bg palms": "./levels/1/level_1_bg_palms.csv",
+        "crates": "./levels/1/level_1_crates.csv",
+        "enemies": "./levels/1/level_1_enemies.csv",
+        "constraints": "./levels/1/level_1_constraints.csv",
+        "player": "./levels/1/level_1_player.csv",
+        "grass": "./levels/1/level_1_grass.csv", 
+        "node_pos": (300, 220),
+        "unlock": 2,
+        "node_graphics": "./graphics/overworld/1"}
+
+level_2 = {
+        "terrain": "./levels/2/level_2_terrain.csv",
+        "coins": "./levels/2/level_2_coins.csv",
+        "fg palms": "./levels/2/level_2_fg_palms.csv",
+        "bg palms": "./levels/2/level_2_bg_palms.csv",
+        "crates": "./levels/2/level_2_crates.csv",
+        "enemies": "./levels/2/level_2_enemies.csv",
+        "constraints": "./levels/2/level_2_constraints.csv",
+        "player": "./levels/2/level_2_player.csv",
+        "grass": "./levels/2/level_2_grass.csv", 
+        "node_pos":(480, 610),
+        "unlock": 3,
+        "node_graphics": "./graphics/overworld/2"}
+
+level_3 = {
+        "terrain": "./levels/3/level_4._terrain.csv",
+        "coins": "./levels/3/level_4._coins.csv",
+        "fg palms": "./levels/3/level_4._fg_palms.csv",
+        "bg palms": "./levels/3/level_4._bg_palms.csv",
+        "crates": "./levels/3/level_4._crates.csv",
+        "enemies": "./levels/3/level_4._enemies.csv",
+        "constraints": "./levels/3/level_4._constraints.csv",
+        "player": "./levels/3/level_4._player.csv",
+        "grass": "./levels/3/level_4._grass.csv", 
+        "node_pos":(610, 350),
+        "unlock": 4,
+        "node_graphics": "./graphics/overworld/3"}
+
+level_4 = {
+        "terrain": "./levels/5/Level_5_terrain.csv",
+        "coins": "./levels/5/Level_5_coins.csv",
+        "fg palms": "./levels/5/Level_5_fg_palms.csv",
+        "bg palms": "./levels/5/Level_5_bg_palms.csv",
+        "crates": "./levels/5/Level_5_crates.csv",
+        "enemies": "./levels/5/Level_5_enemies.csv",
+        "constraints": "./levels/5/Level_5_constraints.csv",
+        "player": "./levels/5/Level_5_player.csv",
+        "grass": "./levels/5/Level_5_grass.csv", 
+        "node_pos":(880, 210),
+        "unlock": 5,
+        "node_graphics": "./graphics/overworld/4"}
+
+level_5 = {
+        "terrain": "./levels/4/_terrain.csv",
+        "coins": "./levels/4/_coins.csv",
+        "fg palms": "./levels/4/_fg_palms.csv",
+        "bg palms": "./levels/4/_bg_palms.csv",
+        "crates": "./levels/4/_crates.csv",
+        "enemies": "./levels/4/_enemies.csv",
+        "constraints": "./levels/4/_constraints.csv",
+        "player": "./levels/4/_player.csv",
+        "grass": "./levels/4/_grass.csv", 
+        "node_pos":(1050, 400),
+        "unlock": 5,
+        "node_graphics": "./graphics/overworld/5"}
+
+levels = {
+        0: level_0,
+        1: level_1,
+        2: level_2,
+        3: level_3,
+        4: level_4,
+        5: level_5}
